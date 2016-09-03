f61c4d4153f04119d7253d52dda01c62948324ee
Luis Alberto Gómez Rodríguez <software@geoenlace.com>
2016-09-03 18:23:04 -0500

    feat(): simple routing between components

--- src/main.js
+++ src/main.js
@@ -8,5 +8,33 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Home = require('./components/homePage');
+var About = require('./components/about/about');
 
-ReactDOM.render(<Home />, document.getElementById('app'));
+(function(win){
+    'use strict';
+
+    var App = React.createClass({
+        render: function(){
+            var Child;
+
+            switch (this.props.route){
+                case '/about': Child = About; break;
+                default: Child = Home;
+            }
+
+            return (
+                <div>
+                    <Child />
+                </div>
+            );
+        }
+    });
+
+    function render(){
+        var route = win.location.hash.substr(1);
+        ReactDOM.render(<App route={route} />, document.getElementById('app'));
+    }
+
+    win.addEventListener('hashchange', render);
+    render();
+})(window);
